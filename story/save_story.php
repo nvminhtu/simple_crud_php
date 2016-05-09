1aba88766ab061b1a05f61814b1c9020dcf223fe
Tu Nguyen <nvminhtu@gmail.com>
2016-05-09 11:08:44 +0700

    refs #update database, save json

--- story/save_story.php
+++ story/save_story.php
@@ -1,20 +1,17 @@
 <?php
-
-$title = mysql_real_escape_string(trim(htmlentities($_REQUEST['title'])));
-//$content = htmlspecialchars($_REQUEST['content']);
-$content = mysql_real_escape_string(trim(htmlentities($_REQUEST['content'])));
+//$title = mysql_real_escape_string(trim(htmlentities($_REQUEST['title'])));
+$title = mysql_real_escape_string(htmlentities(html_entity_decode($_REQUEST['title'])));
+$content = htmlentities(mysql_real_escape_string(trim($_REQUEST['content'])));
 //$content =  mysql_real_escape_string(htmlentities(stripslashes(utf8_encode($_REQUEST['content'])), ENT_QUOTES));
 $cat_id = htmlspecialchars($_REQUEST['cat_id']);
-
 include '../include/conn.php';
-
 $sql = "insert into story(title,content,cat_id) values('$title','$content','$cat_id')";
 $result = @mysql_query($sql);
 if ($result){
 	echo json_encode(array(
 		'id' => mysql_insert_id(),
-		'title' => $title,
-		'content' => $content,
+		'title' => html_entity_decode($title),
+		'content' => html_entity_decode($content),
 		'cat_id' => $cat_id
 	));
 } else {
--- story/update_story.php
+++ story/update_story.php
@@ -1,18 +1,16 @@
 <?php
-
 $id = intval($_REQUEST['id']);
-$title = mysql_real_escape_string(trim(htmlentities($_REQUEST['title'])));
-$content = mysql_real_escape_string(trim(htmlentities(htmlspecialchars(html_entity_decode($_REQUEST['content'])))));
+$title = mysql_real_escape_string(htmlentities(html_entity_decode($_REQUEST['title'])));
+//$content = mysql_real_escape_string(trim(htmlentities(htmlspecialchars(html_entity_decode($_REQUEST['content'])))));
+$content = mysql_real_escape_string(trim(htmlentities(html_entity_decode($_REQUEST['content']))));
 $cat_id = htmlspecialchars($_REQUEST['cat_id']);
-
 include '../include/conn.php';
-
 $sql = "update story set title='$title',content='$content',cat_id='$cat_id' where id=$id";
 $result = @mysql_query($sql);
 if ($result){
 	echo json_encode(array(
 		'id' => $id,
-		'title' => $title,
+		'title' => html_entity_decode($title),
 		'content' => html_entity_decode($content),
 		'cat_id' => $cat_id
 	));
